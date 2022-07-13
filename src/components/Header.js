b558167a2c4996871de43de18d2b340eb78f056d
dima-sheiko <sheydim@gmail.com>
2022-07-13 17:44:22 +0300

    Update basic file structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Header() {
-  return <div>Header</div>;
+  return <div>
+    <h1>Hollow Knight</h1>
+    <h2>Memory Game</h2>
+    <div>Your score:</div>
+  </div>;
 }
 
 export default Header;
